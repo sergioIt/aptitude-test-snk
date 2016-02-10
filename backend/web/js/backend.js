25c6a1d5c0c424e72809246e9166c31c3960e861
Sergio <serge.kite@gmail.com>
2016-02-10 11:43:05 +0300

    backend: js: fix ajax view action route

--- backend/web/js/backend.js
+++ backend/web/js/backend.js
@@ -5,19 +5,15 @@
 
 $('.btn_view_test').click(function() {
     $.get(
-        'test/view',
+        'view',
         {
             test_id: $(this).data('id')
         },
 
         function (data) {
             var modal = $('#activity-modal');
-            var test_id = $(this).data('id');
-            //$('#test_id_container-modal').html($(this).data('id'));
-            console.log(test_id);
             modal.find('.modal-body').html(data);
             modal.modal();
-            //$('#activity-modal').modal();
         }
     );
 });
